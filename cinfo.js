e07f4392eb7013db92a8d6adb0f689fb03d2927d
brindasubedi <subedibrinda@gmail.com>
2020-05-01 22:24:25 +0545

    Delete cinfo.js

--- cinfo.js
+++
@@ -1,8 +0,0 @@
-function load(){
-document.getElementById("customerName").innerHTML= localStorage.getItem("Cname");
-
-document.getElementById("customerAddress").innerHTML= localStorage.getItem("Aname");
-document.getElementById("customerPhone").innerHTML= localStorage.getItem("Pname");
-loaditem();
-amount();
-}
